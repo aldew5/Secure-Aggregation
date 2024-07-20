c127220c9e029a3651c454e0d8c12bbf6d41835f
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2024-07-20 19:44:25 -0400

    mobilenet

--- mobilenet.py
+++ mobilenet.py
@@ -1,21 +1,23 @@
-import keras
+
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Flatten, Input
-from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.optimizers import Adam
 import numpy as np
 
+#mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-
+# need 3 channels
 x_train = np.stack([x_train]*3, axis=-1)
 x_test = np.stack([x_test]*3, axis=-1)
 
-x_train = tf.image.resize(x_train, (224, 224)).numpy()
-x_test = tf.image.resize(x_test, (224, 224)).numpy()
+# smaller, faster, 64x64
+x_train = tf.image.resize(x_train, (64, 64)).numpy()
+x_test = tf.image.resize(x_test, (64, 64)).numpy()
 
 x_train = x_train / 255.0
 x_test = x_test / 255.0
@@ -23,29 +25,22 @@ x_test = x_test / 255.0
 y_train = to_categorical(y_train, 10)
 y_test = to_categorical(y_test, 10)
 
-input_shape = (224, 224, 3)
-
-resnet = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
-
-# freeze
-for layer in resnet.layers:
+input_shape = (64, 64, 3)
+mobilenet = MobileNetV2(weights='imagenet', include_top=False, input_shape=input_shape)
+for layer in mobilenet.layers:
     layer.trainable = False
-
-# tunable layers
 inputs = Input(shape=input_shape)
-x = resnet(inputs, training=False)
+x = mobilenet(inputs, training=False)
 x = Flatten()(x)
-x = Dense(150, activation='relu')(x)
+x = Dense(512, activation='relu')(x)
 outputs = Dense(10, activation='softmax')(x)
-
 model = Model(inputs, outputs)
 
-model.compile(optimizer=Adam(learning_rate=0.01),
+model.compile(optimizer=Adam(learning_rate=0.001),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-              
+
 model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
 
-
-
-
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {accuracy}')
